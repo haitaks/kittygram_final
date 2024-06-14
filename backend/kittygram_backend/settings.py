9648a957a1ff2370f49031d5465cfe121eeb55c2
haitaks <yc-user@epds97289gu84hk1ao5t.auto.internal>
2024-06-14 03:07:22 +0000

    secret

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', '').lower() in ('false', '0')
 
